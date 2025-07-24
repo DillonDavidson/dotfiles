929df871601c0484a022c69c4f67c952ec161281
DillonDavidson <90550872+DillonDavidson@users.noreply.github.com>
2025-07-24 01:02:42 -0700

    more...

--- config/doom/config.el
+++ config/doom/config.el
@@ -35,7 +35,7 @@
 
 ;;; Code:
 
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-one)
 (setq doom-font "JetBrainsMono NF-16")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
