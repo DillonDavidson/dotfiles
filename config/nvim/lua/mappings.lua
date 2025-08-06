bfcf27c616b73eba1f823d6967afba157aa3f0e2
DillonDavidson <90550872+DillonDavidson@users.noreply.github.com>
2025-08-05 21:18:22 -0700

    more

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -5,6 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>z", "<cmd>set foldmethod=indent<CR>", { desc = "Set foldmethod to indent" })
 map({ "n", "t" }, "<A-i>", function()
   require("nvchad.term").toggle {
     pos = "float",
