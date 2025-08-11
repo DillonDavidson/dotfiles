ca3a9ba3ba9c7b800b657d1a2706ed40b6f53909
DillonDavidson <90550872+DillonDavidson@users.noreply.github.com>
2025-08-10 21:45:58 -0700

    monthly update

--- local/.wezterm.lua
+++ local/.wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local act = wezterm.action
 local config = wezterm.config_builder()
 local mux = wezterm.mux
 
-config.default_domain = 'WSL:archlinux'
+config.default_domain = "WSL:archlinux"
 
-wezterm.on('gui-startup', function(window)
+wezterm.on("gui-startup", function(window)
 	local tab, pane, window = mux.spawn_window(cmd or {})
-	local gui_window = window:gui_window();
+	local gui_window = window:gui_window()
 	gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
@@ -25,11 +25,20 @@ wezterm.on("toggle-tabbar", function(window, _)
 end)
 
 config.keys = {
-	{ key = "T", mods = "CTRL", action = act.EmitEvent("toggle-tabbar") },
+	{ key = "t", mods = "ALT", action = act.EmitEvent("toggle-tabbar") },
 }
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.enable_scroll_bar = false
 config.font_size = 16
-config.color_scheme = 'Solarized Light (Gogh)'
+config.color_scheme = "Kanagawa (Gogh)"
+
+config.window_padding = {
+	left = "0cell",
+	right = "0cell",
+	top = "0cell",
+	bottom = "0cell",
+}
 
 return config
